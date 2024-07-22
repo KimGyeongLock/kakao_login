46617f736d2b4272e86482e8c34010ccf27d81b7
LocKey <rudfhr97@naver.com>
2024-07-23 01:09:57 +0900

    DB(MySQL) 연결하기

--- src/main/java/com/example/kakaologin/KakaologinApplication.java
+++ src/main/java/com/example/kakaologin/KakaologinApplication.java
@@ -17,6 +17,9 @@ public class KakaologinApplication {
         System.setProperty("KAKAO_CLIENT_ID", dotenv.get("KAKAO_CLIENT_ID"));
         System.setProperty("KAKAO_CLIENT_SECRET", dotenv.get("KAKAO_CLIENT_SECRET"));
         System.setProperty("KAKAO_REDIRECT_URI", dotenv.get("KAKAO_REDIRECT_URI"));
+        System.setProperty("MYSQL_URL", dotenv.get("MYSQL_URL"));
+        System.setProperty("MYSQL_USERNAME", dotenv.get("MYSQL_USERNAME"));
+        System.setProperty("MYSQL_PASSWORD", dotenv.get("MYSQL_PASSWORD"));
 
         SpringApplication.run(KakaologinApplication.class, args);
     }
